b61d150ae9302af4ed0228ce04f9af4cf674cc70
Spencer Banasik <smbanasik@gmail.com>
2024-12-02 18:34:12 -0600

    Started adaptation on main.

--- Source/main.py
+++ Source/main.py
@@ -1,128 +1,132 @@
-import torch
 import numpy as np
-from PIL import Image
+import random
+import time
+import datetime
+import json
+from pathlib import Path
+
+import torch
+import torchvision
 import torch.nn as nn
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+from torch.utils.data import DataLoader, Subset
+from torchvision import transforms, datasets
 
 from data_process import generate_loaders
 
-def plot_save(train_log, test_log, title, ylabel, filename):
-    plt.rcParams["figure.figsize"] = (9,5)
-    x=np.arange(len(train_log))
-    plt.figure()
-    plt.plot(x, train_log, linestyle='-', label='Train Set', linewidth=3)
-    plt.plot(x, test_log,  linestyle='-', label='Test Set', linewidth=3)
-    plt.title(title,fontsize=25)
-    plt.legend(fontsize=15)
-    plt.grid()
-    plt.ylabel(ylabel, fontsize=25)
-    plt.xlabel('Epoch', fontsize=25)
-    plt.savefig(filename)
+class HyperParamsAndArgs():
+    def __init__(self, **kwargs):
+        self.epochs = 30
+        self.batch_size = 128
+        self.seed = 615
 
-def plot(train_log, test_log, title, ylabel):
-    plt.rcParams["figure.figsize"] = (9,5)
-    x=np.arange(len(train_log))
-    plt.figure()
-    plt.plot(x, train_log, linestyle='-', label='Train Set', linewidth=3)
-    plt.plot(x, test_log,  linestyle='-', label='Test Set', linewidth=3)
-    plt.title(title,fontsize=25)
-    plt.legend(fontsize=15)
-    plt.grid()
-    plt.ylabel(ylabel, fontsize=25)
-    plt.xlabel('Epoch', fontsize=25)
-    plt.show()
+        self.output_dir = "../output"
 
-class HyperParams():
-    def __init__(self):
-        self.epochs = 15
         self.learn_rates = [0.0005, 0.001, 0.002]
         self.learn_rates_pesg = [0.02, 0.05, 0.1]
         self.lr_decay = 0.1
         self.lr_epoch = 30
-        self.batch_size = 128
         self.weight_decay = 0
         self.gammas = [0.2, 0.5, 0.8]
         self.decay_epochs = []
 
 def main():
 
-    parameters = HyperParams()
+    parameters = HyperParamsAndArgs()
+    torch.manual_seed(parameters.seed)
+    np.random.seed(parameters.seed)
+    random.seed(parameters.seed)
+
+    # TODO: make sure this function does what its intended to do
     train_loader, coco_loader, imagenet_loader = generate_loaders(parameters)
 
-    best_lr = [0, 0]
-    for learn_rate in parameters.learn_rates:
-        model = Model_Default(learn_rate, 0.003)
+    # TODO: create model here
+    # Ensure model gets moved to CUDA.
+    model = Model_Default(learn_rate, 0.003)
+    '''
+    model = CLIP(image_encoder=args.image_encoder, text_encoder=args.text_encoder, embed_dim=args.embed_dim, init_model=args.init_model, bsz=args.batch_size_train*args.world_size,
+                  world_size=args.world_size, ita_type=args.ita_type, sogclr_gamma=args.sogclr_gamma, rho_I=args.rho_I, rho_T=args.rho_T, tau_init=args.tau_init,
+                  eta_init=args.eta_init, beta_u=args.beta_u, temp=args.temp, learnable_temp=args.learnable_temp,
+                  vicreg_sim_coeff=args.vicreg_sim_coeff, vicreg_std_coeff=args.vicreg_std_coeff, personalized_tau=args.personalized_tau, 
+                  use_temp_net=args.isogclr_temp_net, alpha=args.alpha, distributed=args.distributed)
+    '''
+    
+    print("--Begin training--")
 
-        print('Begin Training with Learn Rate', learn_rate)
-        print('-'*30)
+    train_log = []
+    test_log = []
 
-        train_log = []
-        test_log = []
-
-        test_best = 0
-        train_list_AUPRC, test_list_AUPRC = [], []
-        train_list_AUROC, test_list_AUROC = [], []
-        for epoch in range(parameters.epochs):
-            if epoch in parameters.decay_epochs:
-                model.opt.update_lr(decay_factor=10)
-                pass
-            
-            train_loss = []
-            model.network.train()
-            for data, targets in trainloader:
-                data, targets = data.cuda(), targets.cuda()
-                preds = model.network(data)
-                preds = torch.sigmoid(preds)
-                loss = model.loss_func(preds, targets.float())
-
-                model.opt.zero_grad()
-                loss.backward()
-                model.opt.step()
-                train_loss.append(loss.item())
-            
-            model.network.eval()
-            train_pred_list = []
-            train_true_list = []
-            for train_data, train_targets in evalloader:
-                train_data = train_data.cuda()
-                train_pred = model.network(train_data)
-                train_pred_list.append(train_pred.cpu().detach().numpy())
-                train_true_list.append(train_targets.numpy())
-            train_true = np.concatenate(train_true_list)
-            train_pred = np.concatenate(train_pred_list)
-            train_ap = auc_prc_score(train_true, train_pred)
-            train_list_AUPRC.append(train_ap)
-            train_auc = auc_roc_score(train_true, train_pred)
-            train_list_AUROC.append(train_auc)
-            train_loss = np.mean(train_loss)
+    test_best = 0
+    train_list_AUPRC, test_list_AUPRC = [], []
+    train_list_AUROC, test_list_AUROC = [], []
+    for epoch in range(parameters.epochs):
+        if epoch in parameters.decay_epochs:
+            model.opt.update_lr(decay_factor=10)
+            pass
         
-            test_pred_list = []
-            test_true_list = [] 
-            for test_data, test_targets in testloader:
-                test_data = test_data.cuda()
-                test_pred = model.network(test_data)
-                test_pred_list.append(test_pred.cpu().detach().numpy())
-                test_true_list.append(test_targets.numpy())
-            test_true = np.concatenate(test_true_list)
-            test_pred = np.concatenate(test_pred_list)
-            val_ap = auc_prc_score(test_true, test_pred)
-            test_list_AUPRC.append(val_ap)
-            val_auc =  auc_roc_score(test_true, test_pred)
-            test_list_AUROC.append(val_auc)
-            model.network.train()
-            if test_best < val_ap:
-                    test_best = val_ap
+        # TODO: Train function from pipeline.py, adapt to our network dataloader model
 
-            print("epoch: %s, train_loss: %.4f, train_auc: %.4f, test_auc: %.4f, lr: %.4f"%(epoch, train_loss, train_auc, val_auc, model.opt.lr ))    
-            train_log.append(train_auc) 
-            test_log.append(val_auc)
+        # TODO: evaluate function
 
-        if(test_best > best_lr[0]):
-            best_lr = [test_best, learn_rate]
+        # TODO: print results
 
-        plot(train_list_AUPRC, test_list_AUPRC, "CrossEntropyLoss PneumoniaMNIST - AUPRC", "AUPRC", "crossent_auprc" + "_lr" + str(learn_rate) + ".png")
-        plot(train_list_AUROC, test_list_AUROC, "CrossEntropyLoss PneumoniaMNIST - AUROC", "AUROC", "crossent_auroc" + "_lr" + str(learn_rate) + ".png")
+        train_loss = []
+        model.network.train()
+        for data, targets in trainloader:
+            data, targets = data.cuda(), targets.cuda()
+            preds = model.network(data)
+            preds = torch.sigmoid(preds)
+            loss = model.loss_func(preds, targets.float())
+
+            model.opt.zero_grad()
+            loss.backward()
+            model.opt.step()
+            train_loss.append(loss.item())
+        
+        model.network.eval()
+        train_pred_list = []
+        train_true_list = []
+        for train_data, train_targets in evalloader:
+            train_data = train_data.cuda()
+            train_pred = model.network(train_data)
+            train_pred_list.append(train_pred.cpu().detach().numpy())
+            train_true_list.append(train_targets.numpy())
+        train_true = np.concatenate(train_true_list)
+        train_pred = np.concatenate(train_pred_list)
+        train_ap = auc_prc_score(train_true, train_pred)
+        train_list_AUPRC.append(train_ap)
+        train_auc = auc_roc_score(train_true, train_pred)
+        train_list_AUROC.append(train_auc)
+        train_loss = np.mean(train_loss)
+    
+        test_pred_list = []
+        test_true_list = [] 
+        for test_data, test_targets in testloader:
+            test_data = test_data.cuda()
+            test_pred = model.network(test_data)
+            test_pred_list.append(test_pred.cpu().detach().numpy())
+            test_true_list.append(test_targets.numpy())
+        test_true = np.concatenate(test_true_list)
+        test_pred = np.concatenate(test_pred_list)
+        val_ap = auc_prc_score(test_true, test_pred)
+        test_list_AUPRC.append(val_ap)
+        val_auc =  auc_roc_score(test_true, test_pred)
+        test_list_AUROC.append(val_auc)
+        model.network.train()
+        if test_best < val_ap:
+                test_best = val_ap
+
+        print("epoch: %s, train_loss: %.4f, train_auc: %.4f, test_auc: %.4f, lr: %.4f"%(epoch, train_loss, train_auc, val_auc, model.opt.lr ))    
+        train_log.append(train_auc) 
+        test_log.append(val_auc)
+
+    if(test_best > best_lr[0]):
+        best_lr = [test_best, learn_rate]
+
+    plot(train_list_AUPRC, test_list_AUPRC, "CrossEntropyLoss PneumoniaMNIST - AUPRC", "AUPRC", "crossent_auprc" + "_lr" + str(learn_rate) + ".png")
+    plot(train_list_AUROC, test_list_AUROC, "CrossEntropyLoss PneumoniaMNIST - AUROC", "AUROC", "crossent_auroc" + "_lr" + str(learn_rate) + ".png")
     print("Best hyper parameters - LR:", best_lr[1])
 
 if __name__ == '__main__':
---
+++ Source/pipeline.py
@@ -0,0 +1,345 @@
+"""
+    Adapted from: https://github.com/zhqiu/contrastive-learning-iSogCLR/blob/main/
+"""
+import numpy as np
+import random
+import time
+import datetime
+import json
+from pathlib import Path
+
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+from torch.utils.data import DataLoader, Subset
+from torchvision import transforms, datasets
+
+from models.model_clip import CLIP
+from transformers import AutoTokenizer, RobertaTokenizer
+
+import utils
+import shutil
+from dataset import create_train_dataset, create_val_dataset, create_sampler, create_train_loader, create_val_loader
+from scheduler import create_scheduler
+from optim import create_optimizer
+from zeroshot_transfer.classes import CIFAR10_CLASSES, CIFAR100_CLASSES, IMAGENET_CLASSES
+
+from tqdm import tqdm
+
+def train(model, data_loader, optimizer, tokenizer, epoch, max_epoch, warmup_steps, device, scheduler, grad_scaler, args):
+    # train
+    model.train()
+    
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    metric_logger.add_meter('lr', utils.SmoothedValue(window_size=1, fmt='{value:.6f}'))
+    metric_logger.add_meter('lr_temp_net', utils.SmoothedValue(window_size=1, fmt='{value:.8f}'))
+    metric_logger.add_meter('loss_ita', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('avg_image_tau', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('avg_text_tau', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('cur_eta', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('grad_tau_image', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('grad_tau_text', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('b_I', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('b_T', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('v', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('lamda', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('weights_image_pos', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+    metric_logger.add_meter('weights_text_pos', utils.SmoothedValue(window_size=1, fmt='{value:.4f}'))
+
+    header = 'Train Epoch: [{}]'.format(epoch)
+    print_freq = 50
+    step_size = 100
+    warmup_iterations = warmup_steps*step_size  
+
+    for i,(image, text, idx, text_idx) in enumerate(metric_logger.log_every(data_loader, print_freq, header)):
+        optimizer.zero_grad()
+
+        image = image.to(device, non_blocking=True)   
+        idx = idx.to(device, non_blocking=True)
+        text_idx = text_idx.to(device, non_blocking=True)   
+        text_input = tokenizer(text, padding='max_length', truncation=True, max_length=30, return_tensors="pt").to(device)
+        
+        # set learning rate for temperature network
+        optimizer.param_groups[2]["lr"] = optimizer.param_groups[0]["lr"] / 10.0
+
+        if grad_scaler is None:
+            loss_ita, info_dict = model(image, text_input, idx=idx, text_idx=text_idx, epoch=epoch, max_epoch=max_epoch)
+            loss_ita.backward()
+            optimizer.step()
+        else:
+            with torch.cuda.amp.autocast():
+                loss_ita, info_dict = model(image, text_input, idx=idx, text_idx=text_idx, epoch=epoch, max_epoch=max_epoch)
+            grad_scaler.scale(loss_ita).backward()
+            grad_scaler.step(optimizer)
+            grad_scaler.update()
+        
+        metric_logger.update(loss_ita=loss_ita.item())
+
+        if args.ita_type in ['sogclr_dro', 'isogclr_new']:
+            metric_logger.update(avg_image_tau=info_dict['avg_image_tau'])
+            metric_logger.update(avg_text_tau=info_dict['avg_text_tau'])
+            metric_logger.update(cur_eta=info_dict['cur_eta'])
+            metric_logger.update(grad_tau_image=info_dict['grad_tau_image'])
+            metric_logger.update(grad_tau_text=info_dict['grad_tau_text'])
+            metric_logger.update(b_I=info_dict['b_I'])
+            metric_logger.update(b_T=info_dict['b_T'])
+            metric_logger.update(weights_image_pos=0.0)
+            metric_logger.update(weights_text_pos=0.0)
+            metric_logger.update(v=0.0)
+            metric_logger.update(lamda=0.0)
+        elif args.ita_type == 'isogclr_new_v2':
+            metric_logger.update(avg_image_tau=info_dict['avg_image_tau'])
+            metric_logger.update(avg_text_tau=info_dict['avg_text_tau'])
+            metric_logger.update(cur_eta=info_dict['cur_eta'])
+            metric_logger.update(grad_tau_image=info_dict['grad_tau_image'])
+            metric_logger.update(grad_tau_text=info_dict['grad_tau_text'])
+            metric_logger.update(b_I=info_dict['b_I'])
+            metric_logger.update(b_T=info_dict['b_T'])
+            metric_logger.update(weights_image_pos=0.0)
+            metric_logger.update(weights_text_pos=0.0)
+            metric_logger.update(v=info_dict['v'])
+            metric_logger.update(lamda=info_dict['lamda'])
+        elif args.ita_type == 'sogclr':
+            metric_logger.update(avg_image_tau=info_dict['avg_image_tau'])
+            metric_logger.update(avg_text_tau=info_dict['avg_text_tau'])
+            metric_logger.update(weights_image_pos=0.0)
+            metric_logger.update(weights_text_pos=0.0)
+            metric_logger.update(cur_eta=0.0)
+            metric_logger.update(grad_tau_image=0.0)
+            metric_logger.update(grad_tau_text=0.0)
+            metric_logger.update(b_I=0.0)
+            metric_logger.update(b_T=0.0)
+            metric_logger.update(v=0.0)
+            metric_logger.update(lamda=info_dict['lamda'])
+        else:
+            metric_logger.update(avg_image_tau=info_dict['avg_image_tau'])
+            metric_logger.update(avg_text_tau=info_dict['avg_text_tau'])
+            metric_logger.update(cur_eta=0.0)
+            metric_logger.update(grad_tau_image=0.0)
+            metric_logger.update(grad_tau_text=0.0)
+            metric_logger.update(weights_image_pos=0.0)
+            metric_logger.update(weights_text_pos=0.0)
+            metric_logger.update(b_I=0.0)
+            metric_logger.update(b_T=0.0)
+            metric_logger.update(v=0.0)
+            metric_logger.update(lamda=0.0)
+
+        metric_logger.update(lr=optimizer.param_groups[0]["lr"])
+        metric_logger.update(lr_temp_net=optimizer.param_groups[2]["lr"])
+        if epoch==0 and i%step_size==0 and i<=warmup_iterations and scheduler is not None: 
+            scheduler.step(i//step_size)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger.global_avg())     
+    return {k: "{:.3f}".format(meter.global_avg) for k, meter in metric_logger.meters.items()}  
+
+
+"""
+    zero-shot transfer
+    https://github.com/goel-shashank/CyCLIP/blob/52d77af2a5f1a4bff01b4c371d6b98e2d0340137/src/evaluate.py#L42
+"""
+def create_zeroshot_dataloader(dataset_name, data_folder, image_size):
+    assert dataset_name in ['cifar10', 'cifar100', 'imagenet']
+
+    if dataset_name == 'cifar10':
+        mean = (0.4914, 0.4822, 0.4465)
+        std = (0.2023, 0.1994, 0.2010)
+    elif dataset_name == 'cifar100':
+        mean = (0.5071, 0.4867, 0.4408)
+        std = (0.2675, 0.2565, 0.2761)
+    else:
+        mean = (0.485, 0.456, 0.406)
+        std = (0.229, 0.224, 0.225)
+
+    normalize = transforms.Normalize(mean=mean, std=std)
+
+    val_transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        normalize,
+    ])
+
+    if dataset_name == 'cifar10':
+        dataset = datasets.CIFAR10(root=data_folder, download=False, train=False, transform=val_transform)
+    elif dataset_name == 'cifar100':
+        dataset = datasets.CIFAR100(root=data_folder, download=False, train=False, transform=val_transform)
+    else:
+        dataset = datasets.ImageFolder(root=data_folder, transform=val_transform)
+
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=512, shuffle=False,
+                                              num_workers=2, pin_memory=True)
+
+    data_loader.num_samples = len(dataset)
+
+    return data_loader
+
+
+
+@torch.no_grad()
+def zeroshot_transfer(model, data_loader, dataset_name, tokenizer, device):
+    model.eval()
+
+    if dataset_name == 'cifar10':
+        config = CIFAR10_CLASSES
+    elif dataset_name == 'cifar100':
+        config = CIFAR100_CLASSES
+    elif dataset_name == 'imagenet':
+        config = IMAGENET_CLASSES
+    else:
+        raise ValueError(f"Unknown dataset: {dataset_name}")
+    classes, templates = config["classes"], config["templates"]
+
+    text_embeddings = []
+    for c in classes:
+        texts = [template(c) for template in templates]
+        text_inputs = tokenizer(texts, padding='max_length', truncation=True, max_length=30, return_tensors="pt").to(device) 
+        text_outputs = model.text_encoder(text_inputs.input_ids, attention_mask=text_inputs.attention_mask, output_hidden_states=False)  
+        text_embeds = F.normalize(model.text_proj(text_outputs.last_hidden_state[:,0,:]), dim=-1)
+        text_embed = text_embeds.mean(dim=0)
+        text_embed /= text_embed.norm()
+        text_embeddings.append(text_embed)
+
+    text_embeddings = torch.stack(text_embeddings, dim=1).to(device)
+
+    topk = [1, 3, 5, 10]
+    correct = {k: 0 for k in topk}
+
+    for image, label in data_loader:
+        image, label = image.to(device), label.to(device)
+        image_feat = model.visual_encoder(image)        
+        image_embed = model.vision_proj(image_feat)            
+        image_embedding = F.normalize(image_embed, dim=-1)
+
+        logits = image_embedding @ text_embeddings
+        ranks = logits.topk(max(topk), 1)[1].T
+        predictions = ranks == label
+
+        for k in topk:
+            correct[k] += torch.sum(torch.any(predictions[:k], dim=0)).item()
+
+    results = {f"zeroshot_top{k}": 100.0 * correct[k] / data_loader.num_samples for k in topk}
+
+    return results
+
+
+
+@torch.no_grad()
+def evaluation(model, data_loader, tokenizer, device, args):
+    # test
+    model.eval() 
+    
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Evaluation:'    
+    
+    print('Computing features for evaluation...')
+    start_time = time.time()  
+
+    texts = data_loader.dataset.text   
+    num_text = len(texts)
+    text_bs = 256
+    text_embeds = []
+    for i in range(0, num_text, text_bs):
+        text = texts[i: min(num_text, i+text_bs)]
+        text_input = tokenizer(text, padding='max_length', truncation=True, max_length=30, return_tensors="pt").to(device) 
+        text_output = model.text_encoder(text_input.input_ids, attention_mask=text_input.attention_mask, output_hidden_states=False)  
+        text_embed = F.normalize(model.text_proj(text_output.last_hidden_state[:,0,:]), dim=-1)
+        text_embeds.append(text_embed)
+    text_embeds = torch.cat(text_embeds,dim=0)
+    
+    image_embeds = []
+    for image, img_id in data_loader: 
+        image = image.to(device) 
+        image_feat = model.visual_encoder(image)        
+        image_embed = model.vision_proj(image_feat)            
+        image_embed = F.normalize(image_embed, dim=-1)      
+        image_embeds.append(image_embed)
+    image_embeds = torch.cat(image_embeds,dim=0)
+    
+    sims_matrix = image_embeds.to(device) @ text_embeds.to(device).t()
+    score_matrix_i2t = torch.full((len(data_loader.dataset.image),len(texts)),-100.0).to(device)
+    
+    num_tasks = utils.get_world_size()
+    rank = utils.get_rank() 
+    step = sims_matrix.size(0)//num_tasks + 1
+    start = rank*step
+    end = min(sims_matrix.size(0),start+step)
+
+    for i,sims in enumerate(sims_matrix[start:end]): 
+        topk_sim, topk_idx = sims.topk(k=args.k_test, dim=0)
+        score_matrix_i2t[start+i, topk_idx] = topk_sim
+        
+    sims_matrix = sims_matrix.t()
+    score_matrix_t2i = torch.full((len(texts),len(data_loader.dataset.image)),-100.0).to(device)
+    
+    step = sims_matrix.size(0)//num_tasks + 1
+    start = rank*step
+    end = min(sims_matrix.size(0),start+step)    
+    
+    for i,sims in enumerate(sims_matrix[start:end]): 
+        topk_sim, topk_idx = sims.topk(k=args.k_test, dim=0)
+        score_matrix_t2i[start+i, topk_idx] = topk_sim
+
+    if args.distributed:
+        dist.barrier()   
+        torch.distributed.all_reduce(score_matrix_i2t, op=torch.distributed.ReduceOp.SUM) 
+        torch.distributed.all_reduce(score_matrix_t2i, op=torch.distributed.ReduceOp.SUM)        
+        
+    total_time = time.time() - start_time
+    total_time_str = str(datetime.timedelta(seconds=int(total_time)))
+    print('Evaluation time {}'.format(total_time_str)) 
+
+    return score_matrix_i2t.cpu().numpy(), score_matrix_t2i.cpu().numpy()
+
+
+            
+@torch.no_grad()
+def itm_eval(scores_i2t, scores_t2i, txt2img, img2txt):
+    
+    #Images->Text 
+    ranks = np.zeros(scores_i2t.shape[0])
+    for index,score in enumerate(scores_i2t):
+        inds = np.argsort(score)[::-1]
+        # Score
+        rank = 1e20
+        for i in img2txt[index]:
+            tmp = np.where(inds == i)[0][0]
+            if tmp < rank:
+                rank = tmp
+        ranks[index] = rank
+
+    # Compute metrics
+    tr1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    tr5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    tr10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
+  
+    #Text->Images 
+    ranks = np.zeros(scores_t2i.shape[0])
+    
+    for index,score in enumerate(scores_t2i):
+        inds = np.argsort(score)[::-1]
+        ranks[index] = np.where(inds == txt2img[index])[0][0]
+
+    # Compute metrics
+    ir1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
+    ir5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
+    ir10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)        
+
+    tr_mean = (tr1 + tr5 + tr10) / 3
+    ir_mean = (ir1 + ir5 + ir10) / 3
+    r_mean = (tr_mean + ir_mean) / 2
+
+    eval_result =  {'txt_r1': tr1,
+                    'txt_r5': tr5,
+                    'txt_r10': tr10,
+                    'txt_r_mean': tr_mean,
+                    'img_r1': ir1,
+                    'img_r5': ir5,
+                    'img_r10': ir10,
+                    'img_r_mean': ir_mean,
+                    'r_mean': r_mean}
+    return eval_result
